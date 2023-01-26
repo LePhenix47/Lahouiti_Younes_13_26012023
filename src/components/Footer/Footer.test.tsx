5ad1c236d45b6af6961991cf5bc969acfbb6b7bf
Younes Lahouiti <loller763@gmail.com>
2023-01-26 14:27:37 +0100

    Ajout de test de rendering pour le header + footer

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -1,2 +1,14 @@
 //Footer component
 import Footer from "./Footer";
+
+//React Testing Library
+import { render, screen } from "@testing-library/react";
+
+describe("The footer component", () => {
+  //
+  it("should render without crashing", async () => {
+    render(<Footer />);
+  });
+
+  //
+});
--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -1,14 +1,10 @@
-/* 
-@jest-environment jsdom
-*/
-
 //Header component
 import Header from "./Header";
 
 //React Testing Library
 import { render, screen } from "@testing-library/react";
 
-describe("The footer component", () => {
+describe("The header component", () => {
   //
   it("should render without crashing", async () => {
     render(<Header />);
